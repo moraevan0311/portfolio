0cdfa7631ca1d6e162415b9b4d4885feed2bb3b8
moraevan0311 <84281974+moraevan0311@users.noreply.github.com>
2026-03-09 23:40:53 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("darkModeToggle");
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        if(document.body.classList.contains("dark-mode")){
+            toggle.textContent = "☀️"; // show sun in dark mode
+        } else {
+            toggle.textContent = "🌙"; // show moon in light mode
+        }
+    });
+});
